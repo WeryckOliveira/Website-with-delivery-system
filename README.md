2c9cebbd1611bc0cf225f21742fc980038bfef9b
Weryck Oliveira <88854403+WeryckOliveira@users.noreply.github.com>
2021-08-13 20:58:50 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <br>
 
 <h2>Description</h2>
-<p>a website developed with the purpose of publicizing the place and making small deliveries around the city, with a dashboard with a customer and order system</p>
+<p>a website developed with the purpose of publicizing the place and making small deliveries around the city, with a dashboard with a customer and order system.</p>
 
 <br>
 <br>
@@ -29,3 +29,42 @@
 <br>
 
 <h2>Getting started</h2>
+<p>How to use the login system :</p>
+<ol>
+    <li>Create a database with the name 'login'</li>
+    <li>Create a table with the name "usuario"</li>
+    <li>Create the columns('ID', 'usuario', 'senha')</li>
+    <li>The values ​​in this table will be the dashboard username and password</li>
+</ol>
+
+<p>How to use the ordering system :</p>
+<ol>
+    <li>Create a database with the name 'pedidos'</li>
+    <li>Create two tables, one named 'lista', and one named 'mystatus'</li>
+    <li>In the list "table" create the following columns('ID', 'nome', 'sobrenome', 'email', 'telefone', 'rua', 'numero', 'cep', 'produtos', 'total', 'horario'), this is where the user's data and products will be</li>
+    <li>In the list "mystatus" create the following columns('ID', 'statush'), this is where order status will be saved</li>
+</ol>
+
+<br>
+<br>
+
+<p align="center">
+    <img width="460" height="360" src="Images/Dashboard.gif">
+</p>
+
+<br>
+<br>
+
+<h2>Links</h2>
+<ul>
+    <li>Repository: (https://github.com/WeryckOliveira/Website-with-delivery-system)</li>
+</ul>
+
+<br>
+<br>
+
+<h2>Authors</h2>
+<ul>
+    <li>Weryck Thomaz Oliveira Dias: (https://github.com/WeryckOliveira)</li>
+</ul>
+<p>Please follow github and join us! Thanks to visiting me and good coding!</p>
--- login/valida.php
+++ login/valida.php
@@ -1,19 +1,19 @@
 <?php
 	session_start();	
-	//Incluindo a conexão com banco de dados
+	//Including the database connection
 	include_once("conexao.php");	
-	//O campo usuário e senha preenchido entra no if para validar
+	//The user and password field filled in enter the if to validate
 	if((isset($_POST['email'])) && (isset($_POST['senha']))){
-		$usuario = mysqli_real_escape_string($conn, $_POST['email']); //Escapar de caracteres especiais, como aspas, prevenindo SQL injection
+		$usuario = mysqli_real_escape_string($conn, $_POST['email']); //Escaping special characters, such as quotes, preventing SQL injection
 		$senha = mysqli_real_escape_string($conn, $_POST['senha']);
 		$senha = md5($senha);
 			
-		//Buscar na tabela usuario o usuário que corresponde com os dados digitado no formulário
+		//Search the user table for the user that matches the data entered in the form
 		$result_usuario = "SELECT * FROM usuarios WHERE email = '$usuario' && senha = '$senha' LIMIT 1";
 		$resultado_usuario = mysqli_query($conn, $result_usuario);
 		$resultado = mysqli_fetch_assoc($resultado_usuario);
 		
-		//Encontrado um usuario na tabela usuário com os mesmos dados digitado no formulário
+		//Found a user in the user table with the same data entered in the form
 		if(isset($resultado)){
 			$_SESSION['usuarioId'] = $resultado['id'];
 			$_SESSION['usuarioNome'] = $resultado['nome'];
@@ -26,14 +26,14 @@
 			}else{
 				header("Location: cliente.php");
 			}
-		//Não foi encontrado um usuario na tabela usuário com os mesmos dados digitado no formulário
-		//redireciona o usuario para a página de login
+		//It wasn't found a user in the user table with the same data typed in the form
+		//redirects user to login page
 		}else{	
-			//Váriavel global recebendo a mensagem de erro
+			//Global variable getting error message
 			$_SESSION['loginErro'] = "Usuário ou senha Inválido";
 			header("Location: index.php");
 		}
-	//O campo usuário e senha não preenchido entra no else e redireciona o usuário para a página de login
+	//The unfilled username and password field enters else and redirects the user to the login page
 	}else{
 		$_SESSION['loginErro'] = "Usuário ou senha inválido";
 		header("Location: index.php");
