6d55730159975377b9a17652cda1223b90fa517e
Weryck Oliveira <88854403+WeryckOliveira@users.noreply.github.com>
2024-11-13 20:25:59 -0300

    Update painel.php

--- login/painel.php
+++ login/painel.php
@@ -24,7 +24,7 @@ $conSts = mysqli_query($conexao, $consultaSts) or die ($mysqli->error);
     <script src="https://kit.fontawesome.com/2ce6de8acf.js" crossorigin="anonymous"></script>
 </head>
 
-<body>a
+<body>aa
 
     <section class="aside-left">
         
