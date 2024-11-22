6799b58476e07c2aab309f101264aefd08a9908a
Weryck Oliveira <88854403+WeryckOliveira@users.noreply.github.com>
2024-11-21 21:19:16 -0300

    Update Form.js

--- JS/Form.js
+++ JS/Form.js
@@ -8,7 +8,7 @@ $(function(){
             $(".modal-check").fadeIn();
         }
     });
-
+4
     $(".close-modal").click(function(){
         $(".blue-vivacity").fadeOut();
         $(".modal-check").fadeOut();
