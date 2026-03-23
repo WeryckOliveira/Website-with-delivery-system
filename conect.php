70c3c1a8233c5ad9d28a7e2af27d6fb2cf887d50
Weryck Oliveira <88854403+WeryckOliveira@users.noreply.github.com>
2026-03-23 20:07:32 -0300

    Update conect.php

--- conect.php
+++ conect.php
@@ -3,7 +3,7 @@ define('HOST', 'localhost');
 define('USUARIO', 'root');
 define('SENHA', '');
 define('DB', 'pedidos');
-
+j
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 
